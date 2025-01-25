c309657d6e957c53289374d496d5a04c4a1f2a04
Nuwan Fernando <nuwaninfo@gmail.com>
2025-01-25 19:56:23 +0200

    Done 1st part of exercise 10 1. react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import { useState } from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
-import "./App.css"
+
+import Header from "./components/Header"
+import MyContainer from "./components/MyContainer"
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom"
+import About from "./components/About"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<MyContainer />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
       <div>App</div>
     </>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,31 @@
 import React from "react"
+import "../styles/header.css"
+import { Link } from "react-router-dom"
 
 function Header() {
-  return <div>Header</div>
+  return (
+    <header>
+      <h1>A heading here</h1>
+      <nav>
+        <div className="nav-wrapper">
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <a className="waves-effect waves-light btn">FI</a>
+            </li>
+            <li>
+              <a className="waves-effect waves-light btn">EN</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
