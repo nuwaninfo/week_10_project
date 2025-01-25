67fc64e91c5f4023f6c4f1efb5a11f8600a19bf4
Nuwan Fernando <nuwaninfo@gmail.com>
2025-01-25 20:22:56 +0200

    Removed not used, default imports form App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-
 import Header from "./components/Header"
 import MyContainer from "./components/MyContainer"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import About from "./components/About"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <BrowserRouter>
