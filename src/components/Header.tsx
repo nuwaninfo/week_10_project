d9e185e73aca19b0e8e0b50508b1d6d82cd537c4
Nuwan Fernando <nuwaninfo@gmail.com>
2025-01-26 14:40:42 +0200

    Done 3. Internationalization / i18n of exersice 10

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
 import "../styles/header.css"
 import { Link } from "react-router-dom"
+import { useTranslation } from "react-i18next"
 
 function Header() {
+  const { t, i18n } = useTranslation()
+
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng)
+  }
   return (
     <header>
       <h1>A heading here</h1>
@@ -9,16 +15,26 @@ function Header() {
         <div className="nav-wrapper">
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/">{t("Home")}</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/about">{t("About")}</Link>
             </li>
             <li>
-              <a className="waves-effect waves-light btn">FI</a>
+              <a
+                className="waves-effect waves-light btn"
+                onClick={() => changeLanguage("fi")}
+              >
+                FI
+              </a>
             </li>
             <li>
-              <a className="waves-effect waves-light btn">EN</a>
+              <a
+                className="waves-effect waves-light btn"
+                onClick={() => changeLanguage("en")}
+              >
+                EN
+              </a>
             </li>
           </ul>
         </div>
--- src/components/MyContainer.tsx
+++ src/components/MyContainer.tsx
@@ -1,5 +1,8 @@
+import { useTranslation } from "react-i18next"
+
 function MyContainer() {
-  return <div>MyContainer</div>
+  const { t, i18n } = useTranslation()
+  return <div>{t("This is the front page")}</div>
 }
 
 export default MyContainer
